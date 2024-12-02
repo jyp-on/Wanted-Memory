26d8e3323dba45e4a7585dff6975a438c6bf4e9a
jyp-on <okmlnsunok@naver.com>
2024-12-02 23:43:07 +0900

    refactor: 디자인 변경

--- js/content.js
+++ js/content.js
@@ -22,6 +22,16 @@ function markVisitedJobs() {
     });
 }
 
+// Create a visual marker for visited job posts
+function createVisitedMark() {
+    const mark = document.createElement('div');
+    mark.className = 'visited-mark';
+    mark.innerHTML = `<svg viewBox="0 0 24 24" width="16" height="16">
+        <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+    </svg>`;
+    return mark;
+}
+
 // Save clicked job ID
 function saveClickedJob(jobId) {
     chrome.storage.local.get(['visitedJobs'], function(result) {
